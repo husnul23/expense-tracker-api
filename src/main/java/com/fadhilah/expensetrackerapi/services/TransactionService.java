a2144fe99945fc15674de89ec72d91586cc2f228
Husnul Fadhilah <36095304+husnul23@users.noreply.github.com>
2021-07-29 18:45:26 +0700

    added transactionserviceimpl

--- src/main/java/com/fadhilah/expensetrackerapi/services/TransactionService.java
+++ src/main/java/com/fadhilah/expensetrackerapi/services/TransactionService.java
@@ -10,11 +10,9 @@ public interface TransactionService {
 
     List<Transaction> fetchAllTransactions(Integer userId, Integer categoryId);
 
-    Transaction fetchTransactionById(Integer userId, Integer categoryId, Integer transactionId) throws
-            EtResourceNotFoundException;
+    Transaction fetchTransactionById(Integer userId, Integer categoryId, Integer transactionId) throws EtResourceNotFoundException;
 
-    Transaction addTransaction(Integer userId, Integer categoryId, Double amount, String note,
-                               Long transactionDate) throws EtBadRequestException;
+    Transaction addTransaction(Integer userId, Integer categoryId, Double amount, String note, Long transactionDate) throws EtBadRequestException;
 
     Transaction updateTransaction(Integer userId, Integer categoryId, Integer transactionId, Transaction transaction) throws
             EtBadRequestException;
