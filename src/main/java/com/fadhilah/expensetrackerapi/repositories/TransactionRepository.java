5daa30627c6970c5ff438509d04b4812c7e13e82
Husnul Fadhilah <36095304+husnul23@users.noreply.github.com>
2021-07-29 18:44:45 +0700

    renamed transational to transaction

--- src/main/java/com/fadhilah/expensetrackerapi/repositories/TransactionRepository.java
+++ src/main/java/com/fadhilah/expensetrackerapi/repositories/TransactionRepository.java
@@ -12,11 +12,9 @@ public interface TransactionRepository {
 
     Transaction findById(Integer userId, Integer categoryId, Integer transactionId) throws EtResourceNotFoundException;
 
-    Integer create(Integer userId, Integer categoryId, Double amount, String note, Long transactionDate) throws
-            EtBadRequestException;
+    Integer create(Integer userId, Integer categoryId, Double amount, String note, Long transactionDate) throws EtBadRequestException;
 
-    void update(Integer userId, Integer categoryId, Integer transactionId, Transaction transaction) throws
-            EtBadRequestException;
+    void update(Integer userId, Integer categoryId, Integer transactionId, Transaction transaction) throws EtBadRequestException;
 
     void removeById(Integer userId, Integer categoryId, Integer transactionId) throws EtResourceNotFoundException;
 }
