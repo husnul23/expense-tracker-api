c6d81403014b5a5b88fc585da3eb192345f329e4
Husnul Fadhilah <36095304+husnul23@users.noreply.github.com>
2021-08-02 10:34:21 +0700

    added filter for allowing url and path

--- src/main/java/com/fadhilah/expensetrackerapi/ExpenseTrackerApiApplication.java
+++ src/main/java/com/fadhilah/expensetrackerapi/ExpenseTrackerApiApplication.java
@@ -1,16 +1,34 @@
 package com.fadhilah.expensetrackerapi;
 
 import com.fadhilah.expensetrackerapi.filters.AuthFilter;
+import org.apache.catalina.filters.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 public class ExpenseTrackerApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExpenseTrackerApiApplication.class, args);
+	}
+
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter() {
+		FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>();
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		// add allowed url, * means for every url
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		// allow for any path
+		source.registerCorsConfiguration("/**", config);
+		registrationBean.setFilter(new CorsFilter());
+		registrationBean.setOrder(0);
+		return registrationBean;
 	}
 
 	@Bean
